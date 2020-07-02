61ae23c2ba90b5a90da44aee0c2fe02e92a68d7d
oleg_dudar <oleg_dudar@epam.com>
2020-07-02 08:18:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Before you start.
+You should have twitter dev account enabled.  
+Create an application in your dev twitter account and get next values from there:
+* consumer key
+* consumer secret
+* access token
+* access token secret
+
+Install tweepy
+```
+pip install tweepy
+```
+
+#Here you go
+Place all required fields from precondition inside the code and you are ready to go.  
+```
+python twitter_sample.py
+```
+
+Go to your twitter account and check result.
---
+++ twitter_sample.py
@@ -0,0 +1,21 @@
+import tweepy
+
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCESS_TOKEN_SECRET'
+
+
+def oauth_init():
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        return auth
+    except Exception as e:
+        return None
+
+
+oauth = oauth_init()
+api = tweepy.API(oauth)
+
+api.update_status('New tweet from Python')
